2092d48ba4babf8e30e49d62628691e920fdbeb3
darrenak403 <ngothanhdat4002@gmail.com>
2025-11-28 23:47:02 +0700

    merge user

---
+++ frontend-user/src/global.d.ts
@@ -0,0 +1,54 @@
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: {
+    src: string;
+    height: number;
+    width: number;
+    blurDataURL?: string;
+  };
+  export default content;
+}
