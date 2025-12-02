b1496a0962df6f138d852372ea9a9d665763d057
darrenak403 <ngothanhdat4002@gmail.com>
2025-12-02 15:06:18 +0700

    feat(auth): add google callback page
    Changes:
    - Create GoogleCallbackPage component
    - Integrate SpinnerGoogleCallback for loading state

---
+++ frontend-user/src/app/(auth)/iam/auth/social/callback/google/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import SpinnerGoogleCallback from "@/components/shared/auth/SpinnerGoogleCallback";
+
+export default function GoogleCallbackPage() {
+  return <SpinnerGoogleCallback />;
+}
